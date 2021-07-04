0d7cf69572d6689959f908a25e4d804b39e03c78
Shayan Khaksar <sh.khaksar95@gmail.com>
2021-07-04 18:05:02 +0200

    Added Genre list to merger.

--- classifier/merger.py
+++ classifier/merger.py
@@ -2,6 +2,14 @@ import pyexcel as pe
 
 # We have used this script to map the Kaggle dataset with our own dataset
 
+genre_list = [
+    'Art', 'Biography', 'Business', 'Children', 'Christian', 'Classics', 'Comics', 'Cookbooks', 'Ebooks', 'Fantasy',
+    'Fiction', 'Graphic Novels', 'Historical Fiction', 'History', 'Horror', 'Memoir', 'Music', 'Mystery', 'Nonfiction',
+    'Poetry', 'Psychology', 'Romance', 'Science', 'Science Fiction', 'Self Help', 'Sports', 'Thriller', 'Travel',
+    'Young Adult'
+]
+
+
 def main():
     sheet = pe.get_sheet(file_name="./classifier/dataset.csv", row_limit=20)
     sheet.name_columns_by_row(0)
